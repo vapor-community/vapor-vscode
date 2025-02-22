07e3a34160747849cfb2f09275ae31324da7a287
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2025-02-22 16:27:06 +0100

    Add Leaf formatter

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,37 @@
 import * as vscode from "vscode";
 import { createNewProject } from "./commands/createNewProject";
+import { LeafFormatter } from "./utilities/leafFormat";
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand("vapor-vscode.createNewProject", createNewProject);
+    // MARK: - Create New Project
+	const createNewProjectDisposable = vscode.commands.registerCommand("vapor-vscode.createNewProject", createNewProject);
+	context.subscriptions.push(createNewProjectDisposable);
 
-	context.subscriptions.push(disposable);
+    // MARK: - Leaf Formatter
+	let leafFormatterDisposable: vscode.Disposable | undefined;
+    const leafFormatter = new LeafFormatter();
+
+    const updateLeafFormatter = () => {
+        const enableLeafFormatter = vscode.workspace.getConfiguration("leaf").get<boolean>("format.enable");
+        if (leafFormatterDisposable) {
+            leafFormatterDisposable.dispose();
+            leafFormatterDisposable = undefined;
+        }
+        if (enableLeafFormatter) {
+            leafFormatterDisposable = vscode.languages.registerDocumentFormattingEditProvider("leaf", leafFormatter);
+            context.subscriptions.push(leafFormatterDisposable);
+        }
+    };
+
+    updateLeafFormatter();
+
+    context.subscriptions.push(
+        vscode.workspace.onDidChangeConfiguration(e => {
+            if (e.affectsConfiguration("leaf.format.enable")) {
+                updateLeafFormatter();
+            }
+        })
+    );
 }
 
 export function deactivate() {}
